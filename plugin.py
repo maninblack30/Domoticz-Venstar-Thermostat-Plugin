f7f102904707e206ad8264debff070f5be9b74c3
David <davidjgriff@gmail.com>
2018-01-01 16:00:36 -0500

    updated to most recent plugin structure. added humidity control support. added system and fan state switches with custom icons. moved unit nums to vars for readability

--- plugin.py
+++ plugin.py
@@ -7,13 +7,25 @@
     <params>
         <param field="Address"  label="Address"  width="200px" required="true"  default="192.168.1.x"/>
         <param field="Port"     label="Port"     width="200px" required="true" default="80"/>
+
         <param field="Mode1"    label="Polling Period (seconds)" width="200px" required="true" default="60"/>
+        
+        <param field="Mode2"    label="Humidity Control"    width="75px">
+            <options>
+                <option label="Yes"  value="True"/>
+                <option label="No" value="False"  default="true" />
+            </options>
+        </param>
+
+
         <param field="Mode6"    label="Debug"    width="75px">
             <options>
                 <option label="True"  value="Debug"/>
                 <option label="False" value="Normal"  default="true" />
             </options>
         </param>
+        
+        
    </params>
 </plugin>
 """
@@ -27,20 +39,56 @@ import json
 import base64
 from datetime import datetime
 import time
+
+
    
 class BasePlugin:
     isConnected = False
+
+    icons = {"venstar_fan_on": "venstar_fan_on icons.zip",
+         "venstar_fan_off": "venstar_fan_off icons.zip",
+         "venstar_cool": "venstar_cool icons.zip",
+         "venstar_heat": "venstar_heat icons.zip"}
+
+    modeUnit = 1
+    fanModeUnit = 2
+    heatSetpointUnit = 3
+    coolSetpointUnit = 4
+    dehumSetpointUnit = 5
+    humSetpointUnit = 11
+    tempUnit = 6
+    tempHumUnit = 7
+    humUnit = 8
+    scheduleUnit = 9
+    awayUnit = 10
+    systemStateUnit = 12
+    fanStateUnit = 13
+
 
 
     def __init__(self):
         return
 
     def onStart(self):
-        if Parameters["Mode6"] == "Debug":
+        if Parameters['Mode6'] == 'Debug':
             Domoticz.Debugging(1)
             Domoticz.Debug("onStart called")
             DumpConfig()            
             DumpSettings()
+
+        Domoticz.Debug("onStart called")
+        Domoticz.Debug("Debug = " + Parameters["Mode6"])
+
+         # load custom images
+        for key, value in self.icons.items():
+            Domoticz.Debug('adding ' + key + ' from file ' + value)
+            if key not in Images:
+                Domoticz.Image(value).Create()
+                Domoticz.Debug("Added icon: " + key + " from file " + value)
+        Domoticz.Debug("Number of icons loaded = " + str(len(Images)))
+        for image in Images:
+            Domoticz.Debug("Icon " + str(Images[image].ID) + " " + Images[image].Name)
+       
 
         self.VenstarConn = Domoticz.Connection(Name="Venstar", Transport="TCP/IP", Protocol="HTTP", Address=Parameters["Address"], Port=Parameters["Port"])
         self.VenstarConn.Connect()
@@ -50,31 +98,47 @@ class BasePlugin:
         Domoticz.Debug("onStop called")
 
     def onConnect(self, Connection, Status, Description):
+
         Domoticz.Debug("onConnect called")
         if (Status == 0):
             self.isConnected = True
-            if (1 not in Devices):
-                Options = "LevelActions:"+stringToBase64("||||")+";LevelNames:"+stringToBase64("Off|Heat|Cool|Auto")+";LevelOffHidden:ZmFsc2U=;SelectorStyle:MA=="
+            if (self.modeUnit not in Devices):
+                #Options = "LevelActions:"+stringToBase64("||||")+";LevelNames:"+stringToBase64("Off|Heat|Cool|Auto")+";LevelOffHidden:ZmFsc2U=;SelectorStyle:MA=="
+                Options = {"LevelActions": "||||",
+                  "LevelNames": "Off|Heat|Cool|Auto",
+                  "LevelOffHidden": "false",
+                  "SelectorStyle": "1"}
                 Domoticz.Device(Name="Mode",  Unit=1, TypeName="Selector Switch", Switchtype=18, Image=16, Options=Options).Create()
-            if (2 not in Devices):
-                Options = "LevelActions:"+stringToBase64("||||")+";LevelNames:"+stringToBase64("Auto|On")+";LevelOffHidden:ZmFsc2U=;SelectorStyle:MA=="
+            if (self.fanModeUnit not in Devices):
+                #Options = "LevelActions:"+stringToBase64("||||")+";LevelNames:"+stringToBase64("Auto|On")+";LevelOffHidden:ZmFsc2U=;SelectorStyle:MA=="
+                Options = {"LevelActions": "||||",
+                  "LevelNames": "Auto|On",
+                  "LevelOffHidden": "false",
+                  "SelectorStyle": "1"}
                 Domoticz.Device(Name="Fan Mode",  Unit=2, TypeName="Selector Switch", Switchtype=18, Image=7, Options=Options).Create()
-            if (3 not in Devices): Domoticz.Device(Name="Heat Setpoint", Unit=3, Type=242, Subtype=1).Create()
-            if (4 not in Devices): Domoticz.Device(Name="Cool Setpoint", Unit=4, Type=242, Subtype=1).Create()
-#            if (5 not in Devices): Domoticz.Device(Name="Dehum Setpoint", Unit=5, Type=244, Subtype=73, Switchtype=7).Create()
-            if (6 not in Devices): Domoticz.Device(Name="Temperature", Unit=6, Type=80, Subtype=5).Create()
-            if (7 not in Devices): Domoticz.Device(Name="Temp + Humidity", Unit=7, Type=82, Subtype=5).Create()
-            if (8 not in Devices): Domoticz.Device(Name="Humidity", Unit=8, Type=81, Subtype=1).Create()
-            if (9 not in Devices): Domoticz.Device(Name="Schedule", Unit=9, Type=244, Subtype=73).Create()
-            if (10 not in Devices): Domoticz.Device(Name="Away Mode", Unit=10, Type=244, Subtype=73).Create()
+            if (self.heatSetpointUnit not in Devices): Domoticz.Device(Name="Heat Setpoint", Unit=3, Type=242, Subtype=1).Create()
+            if (self.coolSetpointUnit not in Devices): Domoticz.Device(Name="Cool Setpoint", Unit=4, Type=242, Subtype=1, Image=16).Create()
+            if(Parameters["Mode2"]):
+                if (self.dehumSetpointUnit not in Devices): Domoticz.Device(Name="Dehum Setpoint", Unit=5, Type=244, Subtype=73, Switchtype=7, Image=11).Create()
+                if (self.humSetpointUnit not in Devices): Domoticz.Device(Name="Hum Setpoint", Unit=11, Type=244, Subtype=73, Switchtype=7, Image=11).Create()
+            if (self.tempUnit not in Devices): Domoticz.Device(Name="Temperature", Unit=6, TypeName="Temperature").Create()
+            if (self.tempHumUnit not in Devices): Domoticz.Device(Name="Temp + Humidity", Unit=7, TypeName="Temp+Hum").Create()
+            if (self.humUnit not in Devices): Domoticz.Device(Name="Humidity", Unit=8, TypeName="Humidity").Create()
+            if (self.scheduleUnit not in Devices): Domoticz.Device(Name="Schedule", Unit=9, TypeName="Switch", Image=13).Create()
+            if (self.awayUnit not in Devices): Domoticz.Device(Name="Away Mode", Unit=10, TypeName="Switch").Create()
+                
+            if (self.systemStateUnit not in Devices): Domoticz.Device(Name="System State", Unit=12, Type=17, Switchtype=17, Image=Images['venstar_fan_off'].ID).Create()
+            if (self.fanStateUnit not in Devices): Domoticz.Device(Name="Fan State", Unit=13, Type=17,  Switchtype=17, Image=Images['venstar_fan_off'].ID).Create()
+            #if (14 not in Devices): Domoticz.Device(Name="Temp Units", Unit=14, TypeName="Text").Create()
         else:
             self.isConnected = False
             Domoticz.Log("Failed to connect ("+str(Status)+") to: "+Parameters["Address"])
             Domoticz.Debug("Failed to connect ("+str(Status)+") to: "+Parameters["Address"]+" with error: "+Description)
 
-    def onMessage(self, Connection, Data, Status, Extra):
+    def onMessage(self, Connection, Data):
         Domoticz.Debug("onMessage called")
-        jsonStr = Data.decode("utf-8", "ignore")
+    
+        jsonStr = Data["Data"].decode("utf-8", "ignore")
 
         if "success" in jsonStr:
             return
@@ -85,90 +149,153 @@ class BasePlugin:
         data = json.loads(jsonStr) # parse json string to dictionary
         UpdateDevice(1,0,str(data['mode']*10))
         UpdateDevice(2,0,str(data['fan']*10))
+        #calc humidity status
+        
+        if(data['hum'] < 25):
+            humStat = 2 #dry
+        elif(data['hum'] > 60):
+            humStat = 3 #wet
+        elif(data['hum'] >= 25 and data['hum'] <= 60):
+            humStat = 1 #comfortable
+        else:
+            humStat = 0 #normal
+
+
         if (data['tempunits'] == 0): # If thermostat is in fahrenheit convert to celcius for domoticz
+            
             UpdateDevice(3,0,str((data['heattemp'] -32)*5/9))
             UpdateDevice(4,0,str((data['cooltemp'] -32)*5/9))
             UpdateDevice(6,0,str((data['spacetemp']-32)*5/9))
-            UpdateDevice(7,0,str((data['spacetemp']-32)*5/9)+";"+str(data['hum_setpoint'])+";1")
+            UpdateDevice(7,0,str((data['spacetemp']-32)*5/9)+";"+str(data['hum'])+";"+str(humStat))
         else:
+            
             UpdateDevice(3,0,str(data['heattemp']))
             UpdateDevice(4,0,str(data['cooltemp']))
             UpdateDevice(6,0,str(data['spacetemp']))
-            UpdateDevice(7,0,str(data['spacetemp'])+";"+str(data['hum_setpoint'])+";1")
-#            UpdateDevice(5,0,str(data['hum_setpoint']))
-        UpdateDevice(8,data['hum_setpoint'],"0")
+            UpdateDevice(7,0,str(data['spacetemp'])+";"+str(data['hum'])+";"+str(humStat))
+
+#       if humidity control enabled, get hum set point
+        if(Parameters["Mode2"]):            
+           UpdateDevice(11,2,str(data['hum_setpoint']) ) #nValue 2 to show percentage
+           UpdateDevice(5,2,str(data['dehum_setpoint']) ) #nValue 2 to show percentage
+                            
+        
+        UpdateDevice(8,data['hum'],"0")
         UpdateDevice(9,data['schedule'],"0")
         UpdateDevice(10,data['away'],"0")
+        
+        systemState = 'Idle'
+        systemStateIcon = 'fan_off'
+
+        if(data['state'] == 1):
+            systemState = 'Heating'
+            systemStateIcon = 'heat'
+        elif (data['state'] == 2):
+            systemState = 'Cooling'
+            systemStateIcon = 'cool'
+        elif (data['state'] == 3):
+            systemState = 'Lockout'
+        elif (data['state'] == 4):
+            systemState = 'Error'
+
+        UpdateDevice(12,0,systemState,systemStateIcon) 
+
+        fanState = 'On' if data['fanstate'] == 1 else 'Off'
+        fanStateIcon = 'fan_on' if data['fanstate'] == 1 else 'fan_off'
+
+        UpdateDevice(13,0,fanState,fanStateIcon)
+   
+
 
     def onCommand(self, Unit, Command, Level, Hue):
         Domoticz.Debug("onCommand called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(int(Level)))
         Domoticz.Debug("mode:"+str(int(Devices[1].sValue))+", fan:"+str(Devices[2].nValue)+", heattemp:"+str(int(float(Devices[3].sValue)*9/5+32))+", cooltemp:"+str(int(float(Devices[4].sValue)*9/5+32)))
 
-        if (Unit == 1): # mode
+        domoTempUnits = Settings["TempUnit"] #1=Farenheit,0=Celsius
+
+        if (Unit == self.modeUnit): # mode
             mode_val = int(Level/10)
             UpdateDevice(Unit,0,Level)
         else:
             mode_val = int(int(Devices[1].sValue)/10)
 
-        if (Unit == 2): # fan mode
+        if (Unit == self.fanModeUnit): # fan mode
             fan_val = int(Level/10)
             UpdateDevice(Unit,0,Level)
         else:
             fan_val = int(int(Devices[2].sValue)/10)
 
-        if (Unit == 3): # heat temp
+        if (Unit == self.heatSetpointUnit): # heat temp
             heat_val = int(Level)
+            heat_val_to_store = heat_val
+            if(domoTempUnits == '1'):
+                heat_val_to_store = (float(heat_val)-32)*5/9
+            
+            UpdateDevice(Unit,0,str(heat_val_to_store))
         else:
             heat_val = int(float(Devices[3].sValue)*9/5+32)
 
-        if (Unit == 4): # cool temp
+        if (Unit == self.coolSetpointUnit): # cool temp
             cool_val = int(Level)
+            cool_val_to_store = cool_val
+            if(domoTempUnits == '1'):
+                cool_val_to_store = (float(cool_val)-32)*5/9
+            UpdateDevice(Unit,0,str(cool_val_to_store))
         else:
             cool_val = int(float(Devices[4].sValue)*9/5+32)
 
-        if (Unit <= 4):
+        if (Unit <= self.coolSetpointUnit):
 
-            params = urllib.parse.urlencode({
-                'mode': mode_val,
-                'fan': fan_val,
-                'heattemp': heat_val,
-                'cooltemp': cool_val
-            }).encode("utf-8")
-            headers = { 'Content-Type': 'application/x-www-form-urlencoded', \
-                        'Content-Length' : "%d"%(len(params)) }
-            self.VenstarConn.Send(params, "POST", "/control", headers)            
+            params = 'mode='+str(mode_val)+'&fan='+str(fan_val)+'&heattemp='+str(heat_val)+'&cooltemp='+str(cool_val)
+            headers = { 'Content-Type': 'application/x-www-form-urlencoded' }
+            self.VenstarConn.Send({'Verb':'POST', 'URL':'/control','Headers':headers, 'Data': str(params)})  
+                  
 
-        if (Unit == 9): # schedule
+        if (Unit == self.scheduleUnit): # schedule
             if (Command == "On"):
-                params = urllib.parse.urlencode({
-                    'schedule': 1
-                }).encode("utf-8")
+                params = 'schedule=1';
                 UpdateDevice(Unit,1,"0")
 
             elif (Command == "Off"):
-                params = urllib.parse.urlencode({
-                    'schedule': 0
-                }).encode("utf-8")
+                params = 'schedule=0';
                 UpdateDevice(Unit,0,"0")
-            headers = { 'Content-Type': 'application/x-www-form-urlencoded', \
-                        'Content-Length' : "%d"%(len(params)) }
-            self.VenstarConn.Send(params, "POST", "/settings", headers)
 
-        if (Unit == 10): # away
+            headers = { 'Content-Type': 'application/x-www-form-urlencoded' }
+
+            self.VenstarConn.Send({'Verb':'POST', 'URL':'/settings','Headers':headers, 'Data': params})
+
+        if (Unit == self.awayUnit): # away
             if (Command == "On"):
-                params = urllib.parse.urlencode({
-                    'away': 1
-                }).encode("utf-8")
+                params = 'away=1'
+               
                 UpdateDevice(Unit,1,"0")
 
             elif (Command == "Off"):
-                params = urllib.parse.urlencode({
-                    'away': 0
-                }).encode("utf-8")
+                params =  'away=0'
                 UpdateDevice(Unit,0,"0")
-            headers = { 'Content-Type': 'application/x-www-form-urlencoded', \
-                        'Content-Length' : "%d"%(len(params)) }
-            self.VenstarConn.Send(params, "POST", "/settings", headers)
+            headers = { 'Content-Type': 'application/x-www-form-urlencoded'}
+
+            self.VenstarConn.Send({'Verb':'POST', 'URL':'/settings','Headers':headers, 'Data': params})
+        
+        if(Unit == self.dehumSetpointUnit): #dehum setpoint
+            if(Level >= 25 and Level <= 99):
+                params = 'dehum_setpoint='+str(int(Level))+'&hum_setpoint='+str(int(float(Devices[11].sValue)))
+
+                UpdateDevice(Unit,0,Level)
+
+                headers = { 'Content-Type': 'application/x-www-form-urlencoded' }
+
+                self.VenstarConn.Send({'Verb':'POST', 'URL':'/settings','Headers':headers, 'Data': params})
+
+        if(Unit == self.humSetpointUnit): #hum setpoint
+            if(Level >= 0 and Level <= 60):
+                params = 'hum_setpoint='+str(int(Level))+'&dehum_setpoint='+str(int(float(Devices[5].sValue)))
+                
+                UpdateDevice(Unit,0,str(Level))
+                
+                headers = { 'Content-Type': 'application/x-www-form-urlencoded' }
+                #self.VenstarConn.Send(params, "POST", "/settings", headers)
+                self.VenstarConn.Send({'Verb':'POST', 'URL':'/settings','Headers':headers, 'Data': params})
 
 
     def onNotification(self, Name, Subject, Text, Status, Priority, Sound, ImageFile):
@@ -189,7 +316,9 @@ class BasePlugin:
                         'Host': Parameters["Address"]+":"+Parameters["Port"], \
                         'User-Agent':'Domoticz/1.0', \
                         'Content-Length' : "%d"%(len(data)) }
-            self.VenstarConn.Send(data, "GET", url)
+
+            self.VenstarConn.Send({"Verb":"GET", "URL":url, "Headers": headers})
+
         else:
             Domoticz.Connect()
 
@@ -208,9 +337,9 @@ def onConnect(Connection, Status, Description):
     global _plugin
     _plugin.onConnect(Connection, Status, Description)
 
-def onMessage(Connection, Data, Status, Extra):
+def onMessage(Connection, Data):
     global _plugin
-    _plugin.onMessage(Connection, Data, Status, Extra)
+    _plugin.onMessage(Connection, Data)
 
 def onCommand(Unit, Command, Level, Hue):
     global _plugin
@@ -248,10 +377,16 @@ def DumpSettings():
         Domoticz.Debug( "'" + x + "':'" + str(Settings[x]) + "'")
     return
 
+def DumpImages():
+    Domoticz.Debug("DumpImages called")
+    for x in Images:
+        Domoticz.Debug( "'" + x + "':'" + str(Images[x]) + "'")
+    return
+
 def stringToBase64(s):
     return base64.b64encode(s.encode('utf-8')).decode("utf-8")
 
-def UpdateDevice(Unit, nValue, sValue):
+def UpdateDevice(Unit, nValue, sValue, imageKey=''):
     # Make sure that the Domoticz device still exists (they can be deleted) before updating it 
     if (Unit in Devices):
         # Update the device before it times out even if the values are the same
@@ -264,9 +399,14 @@ def UpdateDevice(Unit, nValue, sValue):
             timeDiff = datetime.now() - datetime(*(time.strptime(Devices[Unit].LastUpdate,'%Y-%m-%d %H:%M:%S')[0:6]))
 
         timeDiffMinutes = (timeDiff.seconds/60)%60
+        imageKey = 'venstar_'+imageKey
         if (Devices[Unit].nValue != nValue) or (Devices[Unit].sValue != sValue or timeDiffMinutes+5 > int(SensorTimeout)):
-            Devices[Unit].Update(nValue=nValue, sValue=str(sValue))
+            if(imageKey and imageKey in Images):
+                Devices[Unit].Update(nValue=nValue, sValue=str(sValue),Image=Images[imageKey].ID)
+            else:
+                Devices[Unit].Update(nValue=nValue, sValue=str(sValue))
             Domoticz.Log("Update "+str(nValue)+":'"+str(sValue)+"' ("+Devices[Unit].Name+")")
     return
+
 
 
